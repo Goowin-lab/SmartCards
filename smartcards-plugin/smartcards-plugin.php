ba3edee1c161c51004c09d30893ab21c380f6fd2
Christian Zúñiga <info@goowin.co>
2026-04-18 09:12:05 -0500

    Smartcards v3.0.8

--- smartcards-plugin/smartcards-plugin.php
+++ smartcards-plugin/smartcards-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: SmartCards
  * Plugin URI: https://goowin.co
  * Description: Formulario para generar archivos VCF, crea el perfil de contacto con la foto de la portada, foto del perfil, botón de guardar contacto, redes sociales, QR Dinámico y aprobación de perfil, optimización Créditos Smart Cards, notificaciones a los editores, Mis smart cards. Productos en el dashboard, mis smarts cards, ajustes, in-app purchases.
- * Version: 3.0.7a
+ * Version: 3.0.8
  * Author: Goowin
  * Author URI: https://goowin.co
  * Text Domain: smartcards
@@ -353,6 +353,7 @@ if ( ! function_exists( 'sc_get_social_icon_fallback_svg' ) ) {
         $raw_key = strtolower( (string) $network_key );
         $raw_key = preg_replace( '/\.svg$/', '', $raw_key );
         $key = str_replace( [ '-', ' ' ], '_', $raw_key );
+        error_log( 'SOCIAL ICON KEY: ' . $key );
         $aliases = [
             'maps'      => 'googlemaps',
             'google_maps' => 'googlemaps',
